54f625a03e14319e59b580ff3b63070e4a37a9b3
Yuuki Takano <yuuki.takano@tier4.jp>
2022-08-23 10:38:44 +0900

    add pubsub
    Signed-off-by: Yuuki Takano <yuuki.takano@tier4.jp>

---
+++ pubsub/src/my_listener/src/main.rs
@@ -0,0 +1,33 @@
+use safe_drive::{
+    context::Context, error::DynError, logger::Logger, msg::common_interfaces::std_msgs, pr_info,
+};
+
+fn main() -> Result<(), DynError> {
+    // Create a context.
+    let ctx = Context::new()?;
+
+    // Create a node.
+    let node = ctx.create_node("my_listener", None, Default::default())?;
+
+    // Create a subscriber.
+    let subscriber = node.create_subscriber::<std_msgs::msg::String>("my_topic", None)?;
+
+    // Create a logger.
+    let logger = Logger::new("my_listener");
+
+    // Create a selector.
+    let mut selector = ctx.create_selector()?;
+
+    // Add a callback function.
+    selector.add_subscriber(
+        subscriber,
+        Box::new(move |msg| {
+            pr_info!(logger, "receive: {}", msg.data);
+        }),
+    );
+
+    // Spin.
+    loop {
+        selector.wait()?;
+    }
+}
---
+++ pubsub/src/my_talker/src/main.rs
@@ -0,0 +1,35 @@
+use safe_drive::{
+    context::Context, error::DynError, logger::Logger, msg::common_interfaces::std_msgs, pr_info,
+};
+use std::time::Duration;
+
+fn main() -> Result<(), DynError> {
+    // Create a context.
+    let ctx = Context::new()?;
+
+    // Create a node.
+    let node = ctx.create_node("my_talker", None, Default::default())?;
+
+    // Create a publisher.
+    let publisher = node.create_publisher::<std_msgs::msg::String>("my_topic", None)?;
+
+    // Create a logger.
+    let logger = Logger::new("my_talker");
+
+    let mut cnt = 0; // Counter.
+    let mut msg = std_msgs::msg::String::new().unwrap();
+    loop {
+        // Create a message to be sent.
+        let data = format!("Hello, World!: cnt = {cnt}");
+        msg.data.assign(&data);
+
+        pr_info!(logger, "send: {}", msg.data); // Print log.
+
+        // Send a message.
+        publisher.send(&msg)?;
+
+        // Sleep 1s.
+        cnt += 1;
+        std::thread::sleep(Duration::from_secs(1));
+    }
+}
