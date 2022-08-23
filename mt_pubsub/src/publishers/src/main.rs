d4fb4fc10f9c873f221ea8ae035ad63f56dc84c2
Yuuki Takano <yuuki.takano@tier4.jp>
2022-08-23 11:06:57 +0900

    add mt_pubsub
    Signed-off-by: Yuuki Takano <yuuki.takano@tier4.jp>

---
+++ mt_pubsub/src/publishers/src/main.rs
@@ -0,0 +1,38 @@
+use safe_drive::{context::Context, error::DynError, msg::common_interfaces::std_msgs};
+use std::time::Duration;
+
+#[async_std::main]
+async fn main() -> Result<(), DynError> {
+    // Create a context and a node.
+    let ctx = Context::new()?;
+    let node = ctx.create_node("publishers", None, Default::default())?;
+
+    // Create publishers.
+    let publisher1 = node.create_publisher::<std_msgs::msg::String>("topic1", None)?;
+    let publisher2 = node.create_publisher::<std_msgs::msg::String>("topic2", None)?;
+
+    // Create a task which sends "Hello, World!".
+    let task1 = async_std::task::spawn(async move {
+        let mut msg = std_msgs::msg::String::new().unwrap();
+        msg.data.assign("Hello, World!");
+        for _ in 0..50 {
+            publisher1.send(&msg).unwrap(); // Send a message.
+            async_std::task::sleep(Duration::from_millis(500)).await; // Sleep 500ms.
+        }
+    });
+
+    // Create a task which sends "Hello, Universe!".
+    let task2 = async_std::task::spawn(async move {
+        let mut msg = std_msgs::msg::String::new().unwrap();
+        msg.data.assign("Hello, Universe!");
+        for _ in 0..100 {
+            publisher2.send(&msg).unwrap(); // Send a message.
+            async_std::task::sleep(Duration::from_millis(250)).await; // Sleep 250ms.
+        }
+    });
+
+    task1.await;
+    task2.await;
+
+    Ok(())
+}
---
+++ mt_pubsub/src/subscribers/src/main.rs
@@ -0,0 +1,55 @@
+use safe_drive::{
+    context::Context, error::DynError, logger::Logger, msg::common_interfaces::std_msgs, pr_info,
+    topic::subscriber::Subscriber,
+};
+
+#[async_std::main]
+async fn main() -> Result<(), DynError> {
+    // Create a context and a node.
+    let ctx = Context::new()?;
+    let node = ctx.create_node("subscribers", None, Default::default())?;
+
+    // Create subscribers.
+    let subscriber1 = node.create_subscriber::<std_msgs::msg::String>("topic1", None)?;
+    let subscriber2 = node.create_subscriber::<std_msgs::msg::String>("topic2", None)?;
+
+    // Receive messages.
+    let task1 = async_std::task::spawn(receiver(subscriber1));
+    let task2 = async_std::task::spawn(receiver(subscriber2));
+
+    task1.await?;
+    task2.await?;
+
+    Ok(())
+}
+
+async fn _receiver(mut subscriber: Subscriber<std_msgs::msg::String>) -> Result<(), DynError> {
+    let logger = Logger::new(subscriber.get_topic_name());
+
+    loop {
+        // Receive a message
+        let msg = subscriber.recv().await?;
+        pr_info!(logger, "{}", msg.data);
+    }
+}
+
+use async_std::future::timeout;
+use safe_drive::pr_warn;
+use std::time::Duration;
+async fn receiver(mut subscriber: Subscriber<std_msgs::msg::String>) -> Result<(), DynError> {
+    let logger = Logger::new(subscriber.get_topic_name());
+
+    loop {
+        // Receive a message with 3s timeout.
+        match timeout(Duration::from_secs(3), subscriber.recv()).await {
+            Ok(result) => pr_info!(logger, "received: {}", result?.data),
+            Err(_) => {
+                // Timed out. Finish receiving.
+                pr_warn!(logger, "timeout");
+                break;
+            }
+        }
+    }
+
+    Ok(())
+}
