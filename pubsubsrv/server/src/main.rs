46a9f7abdb2f2727778ca58e94ee309aa464f862
Yuuki Takano <yuuki.takano@tier4.jp>
2022-12-06 13:56:21 +0900

    update pubsubsrv
    Signed-off-by: Yuuki Takano <yuuki.takano@tier4.jp>

--- pubsubsrv/server/src/main.rs
+++ pubsubsrv/server/src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use safe_drive::{context::Context, error::DynError, msg::common_interfaces::std_srvs};
+
+fn main() -> Result<(), DynError> {
+    // Create a context.
+    let ctx = Context::new()?;
+
+    // Create a node.
+    let node = ctx.create_node("pubsubsrv_server", None, Default::default())?;
+
+    // Create a server.
+    let server = node.create_server::<std_srvs::srv::Empty>("pubsubsrv_service", None)?;
+
+    // Create a selector.
+    let mut selector = ctx.create_selector()?;
+
+    selector.add_server(
+        server,
+        Box::new(move |_msg, _header| std_srvs::srv::EmptyResponse::new().unwrap()),
+    );
+
+    loop {
+        selector.wait()?; // Spin.
+    }
 }
--- pubsubsrv/talker/src/main.rs
+++ pubsubsrv/talker/src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use safe_drive::{context::Context, error::DynError, msg::common_interfaces::std_msgs};
+use std::time::Duration;
+
+fn main() -> Result<(), DynError> {
+    // Create a context.
+    let ctx = Context::new()?;
+
+    // Create a node.
+    let node = ctx.create_node("pubsubsrv_talker", None, Default::default())?;
+
+    // Create a publisher.
+    let publisher = node.create_publisher::<std_msgs::msg::Empty>("pubsubsrv_topic", None)?;
+
+    let msg = std_msgs::msg::Empty::new().unwrap();
+    loop {
+        // Send a message.
+        publisher.send(&msg)?;
+
+        // Sleep 1 sec.
+        std::thread::sleep(Duration::from_secs(1));
+    }
 }
